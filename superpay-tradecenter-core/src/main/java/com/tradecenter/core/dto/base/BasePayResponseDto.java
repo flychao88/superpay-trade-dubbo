2994ef03b8efd9302eda49bfdf9d18af035eaaab
wzs <wangzs@chinagpay.com>
2019-10-11 18:31:29 +0800

    增加基础请求类,主扫，被扫请求类

---
+++ superpay-tradecenter-core/src/main/java/com/tradecenter/core/dto/base/BasePayResponseDto.java
@@ -0,0 +1,13 @@
+package com.tradecenter.core.dto.base;
+
+/**
+ * 项目名称：superpay-trade-dubbo
+ * 类 名 称：BasePayResponseDto
+ * 类 描 述：支付结果响应类
+ * 创建时间：2019/10/11 18:30
+ *
+ * @author：wzs
+ */
+public class BasePayResponseDto extends BaseResponseDto {
+    private static final long serialVersionUID = 3490771299260299388L;
+}
