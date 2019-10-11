faca54470e2fa8ba408ea36dbf408e5171f58290
wzs <wangzs@chinagpay.com>
2019-10-11 17:51:30 +0800

    增加基础请求类,主扫，被扫请求类

---
+++ superpay-tradecenter-core/src/main/java/com/tradecenter/core/dto/base/BaseResponseDto.java
@@ -0,0 +1,27 @@
+package com.tradecenter.core.dto.base;
+
+import java.io.Serializable;
+
+/**
+ * 项目名称：superpay-trade-dubbo
+ * 类 名 称：BaseResponseDto
+ * 类 描 述：基础响应对象
+ * 创建时间：2019/10/11 17:30
+ *
+ * @author：wzs
+ */
+public class BaseResponseDto implements Serializable {
+    private static final long serialVersionUID = -2366298166224257930L;
+
+    /**
+     * 响应码
+     */
+    public String returnCode;
+
+    /**
+     * 响应信息
+     */
+    public String returnMsg;
+
+
+}
